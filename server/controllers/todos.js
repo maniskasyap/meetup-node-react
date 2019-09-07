418e48756927ce8aa99a36f60c5429d300b879f2
Zukhruf Mohamed <zo_ok@ymail.com>
2019-09-08 00:22:10 +0400

    Changing response structure

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -10,15 +10,23 @@ module.exports = {
   create: function(req, res) {
     const id = uuidv1();
 
-    // Add a post
-    db.get('posts')
-      .push({ id, title: req.body})
+    // Async call to update the database with the new post
+    async function dbupdate(){
+    await db.get('posts')
+      .push({ [id]: req.body.text})
       .write()
+    }
+    // If the DB update is successfull, send posts back else error
+    dbupdate().then(()=>{
+        let dbUpdated = db.get('posts');
+        res.send({status:"success",posts:dbUpdated});
+    }).catch((err)=>{
+      res.send({status:"Update failed",error:err})
+    });
 
     // Increment count
     db.update('count', n => n + 1)
     .write()
-    res.send(`ToDo created with id:${id}`);
   },
   read: function(req, res) {
     var todoId = req.params.todoId;
