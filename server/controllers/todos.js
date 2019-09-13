aa7ec8cd449aaa6d113518e5b00a1c2843a92576
Zukhruf Mohamed <zo_ok@ymail.com>
2019-09-13 22:25:00 +0400

    read-todo

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -2,9 +2,10 @@ module.exports = {
   create: function(req, res) {
     res.send('this is to create a todo item');
   },
+
   read: function(req, res) {
     var todoId = req.params.todoId;
-
+    var todoExisting = db.get('posts').value();
     var response = {
       status: 'success',
       data: {
@@ -15,18 +16,24 @@ module.exports = {
     var todos = [];
 
     if (!todoId) {
-      todos.push({ value: 'all todos' });
+      todos.push({ value: todoExisting });
     } else {
-      todos.push({ value: 'todo with id = ' + todoId });
-    }
+     todoExisting.forEach(val => {
+          if(val[todoId]){
+            todos.push({value: val})
+          }
+        });
+        }
 
     response.data.todos = todos.slice();
-
+    
     res.send(response);
   },
+
   update: function(req, res) {
     res.send('this is to update a todo item');
   },
+
   delete: function(req, res) {
     res.send('this is to delete a todo item');
   },
