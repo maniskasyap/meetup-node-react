6772a36d64ec06f23dda2cc5455617eb3d6f0d2c
Zukhruf Mohamed <zo_ok@ymail.com>
2019-09-13 22:10:53 +0400

    todosRouter already configured in app.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var todos = require('./todos');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.use('/',todos);
 
 module.exports = router;
