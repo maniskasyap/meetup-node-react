326105661245fb048524c217668af82313de1b2b
Zukhruf Mohamed <zo_ok@ymail.com>
2019-09-14 15:35:02 +0400

    update todo

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -4,7 +4,7 @@ var todos = require('../controllers/todos');
 
 router.post('/', todos.create);
 router.get('/:todoId?', todos.read);
-router.put('/', todos.update);
+router.put('/:todoId', todos.update);
 router.delete('/:todoId', todos.delete);
 
 module.exports = router;
